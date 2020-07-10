07cfc37d59d6fa4f216124064c327b9c55933be1
seanzhan0319 <seanzhan0319@hotmail.com>
2020-07-09 23:04:56 -0400

    Updates

---
+++ precache-manifest.3a400d06c3b878ff3412fb10a480ff52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2900899cb323c2265396dfa32b757c47",
+    "url": "/survey-builder/index.html"
+  },
+  {
+    "revision": "5c3bb66c5517f27aa794",
+    "url": "/survey-builder/static/css/main.bf9965ff.chunk.css"
+  },
+  {
+    "revision": "656e260b5ad680a45c48",
+    "url": "/survey-builder/static/js/2.f88b969d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/survey-builder/static/js/2.f88b969d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3bb66c5517f27aa794",
+    "url": "/survey-builder/static/js/main.a0052d2b.chunk.js"
+  },
+  {
+    "revision": "e837850a688d91310f9c",
+    "url": "/survey-builder/static/js/runtime-main.8a08f172.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.988bcfcf23cc7ed307516bd449e95684.js"
+  "/survey-builder/precache-manifest.3a400d06c3b878ff3412fb10a480ff52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/survey-builder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
